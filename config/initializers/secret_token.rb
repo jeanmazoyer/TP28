aa4861d552c913424bd97832d8e26bcab9849ade
Jean Mazoyer <jeanmazoyer@gmail.com>
2014-06-28 13:57:42 +0200

    Initialize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TP28::Application.config.secret_token = '22557c7f2b65ccf895d5ac1f299b36b8f59ad5798f5f046b77133031a6e7064fde1a6166c4fb1c30719727f7bb9f77ac1a1020deb690b29877f2396ae5cfddf8'
